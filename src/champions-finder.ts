c235242f45345412961db02afaade879aae69701
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-11-27 21:35:04 +0100

    :white_check_mark: add basic tests

--- src/champions-finder.ts
+++ src/champions-finder.ts
@@ -2,10 +2,21 @@
 interface ChessPlayer {
   age: number;
   elo: number;
+  // check username is uniq
+  username: string;
 }
 
+/*
+TODO
+-- make sure username is uniq
+-- make sure chessPlayers is sanitized
+*/
+
 export default function findChampions(chessPlayers: ChessPlayer[]) : ChessPlayer[] {
-  return chessPlayers;
+  //const sanitizedChessPlayers = chessPlayers.filter((chessPlayer) => checkAge(chessPlayer) && checkElo(chessPlayer));
+
+  const champions = chessPlayers.filter(chessPlayer => isChampion(chessPlayer, chessPlayers));
+  return champions;
 }
 
 export function checkAge(chessPlayer: ChessPlayer) : boolean {
@@ -15,4 +26,13 @@ export function checkAge(chessPlayer: ChessPlayer) : boolean {
 // based on chess.com you cannot have an ELO score below the threshold 100
 export function checkElo(chessPlayer: ChessPlayer) : boolean {
   return chessPlayer.elo >= 100;
+}
+
+export function isChampion(chessPlayerToCompare: ChessPlayer, chessPlayers: ChessPlayer[]): boolean {
+  const foundBetterPlayer = chessPlayers.find((chessPlayerCompared) => {
+    return ((chessPlayerCompared.elo > chessPlayerToCompare.elo && chessPlayerCompared.age <= chessPlayerToCompare.age) ||
+           (chessPlayerCompared.age < chessPlayerToCompare.age && chessPlayerCompared.elo >= chessPlayerToCompare.elo) ) &&
+           chessPlayerToCompare.username !== chessPlayerCompared.username
+  });
+  return !foundBetterPlayer;
 }
--- tests/champions-finder.test.ts
+++ tests/champions-finder.test.ts
@@ -6,25 +6,66 @@ describe('testing findChampions function', () => {
       findChampions([])
     ).toStrictEqual([]);
   });
+
+  test('two players with the same age should return the player with the highest ELO score', () => {
+    const chessPlayers = [
+      {username: "John", elo: 1500, age: 18},
+      {username: "Pascale", elo: 1800, age: 18}
+    ];
+    expect(
+      findChampions(chessPlayers)
+    ).toEqual([{username: "Pascale", elo: 1800, age: 18}]);
+  });
+
+   test('two players with the same age and the same score should return both', () => {
+    const chessPlayers = [
+      {username: "John", elo: 1800, age: 18},
+      {username: "Pascale", elo: 1800, age: 18}
+    ];
+    expect(
+      findChampions(chessPlayers)
+    ).toEqual(chessPlayers);
+  });
+
+  test('two players with the same ELO score and not the same age should return both ', () => {
+    const chessPlayers = [
+      {username: "John", elo: 1500, age: 19},
+      {username: "Pascale", elo: 1500, age: 18}
+    ];
+    expect(
+      findChampions(chessPlayers)
+    ).toEqual([{username: "Pascale", elo: 1500, age: 18}]);
+  });
+
+  test('two players should return the player with the higher score', () => {
+    const chessPlayers = [
+      {username: "John", elo: 1800, age: 19},
+      {username: "Pascale", elo: 1500, age: 18}
+    ];
+    expect(
+      findChampions(chessPlayers)
+    ).toEqual(chessPlayers);
+  });
+
 });
 
 describe('testing check age function', () => {
   test('a valid age should return true', () => {
     expect(
-      checkAge({elo: 12, age: 33})
+      checkAge({username: "John", elo: 12, age: 33})
     ).toBe(true);
   });
 
   test('a negative age should return false', () => {
     expect(
-      checkAge({elo: 12, age: -15})
+      checkAge({username: "John", elo: 12, age: -15})
     ).toBe(false);
   });
 
 
   test('an age equal to 0 should return false', () => {
     expect(
-      checkAge({elo: 12, age: 0})
+      checkAge({username: "John", elo: 12, age: 0})
     ).toBe(false);
   });
 });
@@ -32,26 +73,26 @@ describe('testing check age function', () => {
 describe('testing check ELO function', () => {
   test('a valid ELO score should return true', () => {
     expect(
-      checkElo({elo: 1500, age: 33})
+      checkElo({username: "John", elo: 1500, age: 33})
     ).toBe(true);
   });
 
   test('an ELO score below the threshold should return false', () => {
     expect(
-      checkElo({elo: 99, age: 33})
+      checkElo({username: "John", elo: 99, age: 33})
     ).toBe(false);
   });
 
    test('a negative ELO score should return false', () => {
     expect(
-      checkElo({elo: -150, age: 33})
+      checkElo({username: "John", elo: -150, age: 33})
     ).toBe(false);
   });
 
 
   test('an ELO score equal to 100 should return true', () => {
     expect(
-      checkElo({elo: 100, age: 33})
+      checkElo({username: "John", elo: 100, age: 33})
     ).toBe(true);
   });
 });
